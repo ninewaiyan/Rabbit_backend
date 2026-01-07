82e3b9d06978e4887010a78750b23f237edba869
ninenine <ninewaiyan@evercomm.services>
2026-01-07 17:19:14 +0630

    login and register

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 9000;
+dotenv.config();
+
+console.log(process.env.PORT)
+
+const PORT = process.env.PORT || 3000;
+
+//Connect  to MongoDB
+connectDB();
+
+// API Routes
+app.use("/api/users",userRoutes)
 
 app.get("/" , (req,res)=>{
     res.send("WELCOME TO RABBIT API !")
 })
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
