97099d57af7004e07f64d518323ea9eeca56950d
ninenine <ninewaiyan@evercomm.services>
2026-01-13 10:44:00 +0630

    Cart finish

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -330,5 +330,41 @@ router.get("/similar/:id", async (req, res) => {
 });
 
 
+// @route  DELETE /api/cart
+// @desc Remove a product from the cart
+// @access Public
+router.delete("/",async(req,res)=>{
+const {productId , size ,color,guestId,userId} = req.body;
+
+try{
+    let cart = await getCert(userId,guestId);
+
+    if(!cart) return res.status(404).json({message:"cart not found"});
+
+    const  productIndex = cart.products.findIndex(
+        (p)=>p.productId.toString()===productId &&  
+        p.size === size &&
+        p.color === color
+    );
+
+    if(productIndex > -1){
+        cart.products.splice(productIndex,1);
+        cart.totalPrice = cart.products.reduce(
+            (acc,item)=>acc + item.price * item.quantity,0
+        );
+        await cart.save();
+        return res.status(200).json(cart);
+
+    }else{
+        return res.status(404).json({message:"Product not found in cart"});
+    }
+
+}catch(error){
+    console.error(error);
+    return res.status(500).json({message:"Server Error"})
+
+}
+
+})
 
 module.exports = router;
